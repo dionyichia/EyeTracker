5e10c8573646c13e818e35734ee86d9acff2d611
dionyichia <dionyi2002@gmail.com>
2025-05-06 21:11:46 +0800

    started on core code integration, packaged into classes

--- core/tracker.py
+++ core/tracker.py
@@ -2,114 +2,118 @@ import time
 import sys
 import serial
 
-# Connect to Arduino
-def connect_to_arduino(port, baud_rate):
-    try:
-        arduino = serial.Serial(port, baud_rate)
-        return arduino
-    except serial.SerialException as e:
-        print(f"Unable to connect to port: {e}")
-        sys.exit(1)
-    
-def read_button_state():
-    # Read data from Arduino
-    button_state = arduino.readline().decode().strip()
+class ArduinoTracker():
 
-    # Process button state
-    if button_state == 'ON':
-        print("Button is pressed")
+    # Connect to Arduino
+    def connect_to_arduino(self, port, baud_rate):
+        try:
+            arduino = serial.Serial(port, baud_rate)
+            return arduino
+        except serial.SerialException as e:
+            print(f"Unable to connect to port: {e}")
+            sys.exit(1)
+        
+    def read_button_state(self):
+        # Read data from Arduino
+        button_state = arduino.readline().decode().strip()
 
-        return True
-    elif button_state == '0FF':
-        print("Button is not pressed")
-        return False
+        # Process button state
+        if button_state == 'ON':
+            print("Button is pressed")
 
-def readtime(cur_time):
-    click_time = time.time() - cur_time   
-    return click_time
-
-
-def check_connection(arduino):
-    try:
-        time.sleep(2)
-        arduino = connect_to_arduino('/dev/cu.usbserial-A50285BI', 115200)
-
-        arduino.write(("PING\n").encode('utf-8'))
-        time.sleep(1)  # Wait a moment for Arduino to process
-
-        ##remve line 39 .inwaiting check what outputs in serail ////////////////////////////////////
-
-        if arduino.in_waiting > 0:
-            response = arduino.readline().decode().strip()
-            if response == "PONG":
-                print("Arduino is connected and responding.")
-                return True
-            else:
-                print(f"Unexpected response from Arduino: {response}catch\n")
-                return False
-        else:
-            print("No response from Arduino.")
+            return True
+        elif button_state == '0FF':
+            print("Button is not pressed")
             return False
 
-    except serial.SerialException as e:
-        print(f"Error while checking connection: {e}")
-        return False
+    def readtime(self, cur_time):
+        click_time = time.time() - cur_time   
+        return click_time
 
-def buzzer(arduino, command, prev_command):
-    # Check if the Arduino is properly connected before sending commands
-    #if not check_connection(port, baudrate):
-        #print("Failed to communicate with Arduino. Check the connection.")
-        #return
-    
-    #time.sleep(1)
-    #arduino = connect_to_arduino(port, baudrate)
 
-    # Send the command if connection check passes
+    def check_connection(self, arduino):
+        try:
+            time.sleep(2)
+            arduino = self.connect_to_arduino('/dev/cu.usbserial-A50285BI', 115200)
 
-    print(f"Sending command: {command}")
-    if command in ['H', 'L']:
-        if command != prev_command:
-            try:
-                if command == 'H':
-                    arduino.write(('H').encode('utf-8'))  # Send the command to Arduino
+            arduino.write(("PING\n").encode('utf-8'))
+            time.sleep(1)  # Wait a moment for Arduino to process
+
+            ##remve line 39 .inwaiting check what outputs in serail ////////////////////////////////////
+
+            if arduino.in_waiting > 0:
+                response = arduino.readline().decode().strip()
+                if response == "PONG":
+                    print("Arduino is connected and responding.")
+                    return True
                 else:
-                    arduino.write(('L').encode('utf-8'))  # Send the command to Arduino 
-                arduino.flush() 
+                    print(f"Unexpected response from Arduino: {response}catch\n")
+                    return False
+            else:
+                print("No response from Arduino.")
+                return False
 
-                # Wait for acknowledgment
-                start_time = time.time()
-                while time.time() - start_time < 1:  # 1 second timeout
-                    if arduino.in_waiting > 0:
-                        response = arduino.readline().decode('utf-8').strip()
-                        if response == 'O':
-                            print(f"Command '{command}' acknowledged by Arduino.")
-                            return 1
-                        else:
-                            print(f"Response: '{response}' by Arduino.")
-                            return 2
-                    
-                print(f"No acknowledgment received for command '{command}'.")
-                return 0
-            except serial.SerialException as e:
-                print(f"Error sending command: {e}")
-                return 0
+        except serial.SerialException as e:
+            print(f"Error while checking connection: {e}")
+            return False
+
+    def buzzer(self, arduino, command, prev_command):
+        # Check if the Arduino is properly connected before sending commands
+        #if not check_connection(port, baudrate):
+            #print("Failed to communicate with Arduino. Check the connection.")
+            #return
+        
+        #time.sleep(1)
+        #arduino = connect_to_arduino(port, baudrate)
+
+        # Send the command if connection check passes
+
+        print(f"Sending command: {command}")
+        if command in ['H', 'L']:
+            if command != prev_command:
+                try:
+                    if command == 'H':
+                        arduino.write(('H').encode('utf-8'))  # Send the command to Arduino
+                    else:
+                        arduino.write(('L').encode('utf-8'))  # Send the command to Arduino 
+                    arduino.flush() 
+
+                    # Wait for acknowledgment
+                    start_time = time.time()
+                    while time.time() - start_time < 1:  # 1 second timeout
+                        if arduino.in_waiting > 0:
+                            response = arduino.readline().decode('utf-8').strip()
+                            if response == 'O':
+                                print(f"Command '{command}' acknowledged by Arduino.")
+                                return 1
+                            else:
+                                print(f"Response: '{response}' by Arduino.")
+                                return 2
+                        
+                    print(f"No acknowledgment received for command '{command}'.")
+                    return 0
+                except serial.SerialException as e:
+                    print(f"Error sending command: {e}")
+                    return 0
+            else:
+                return 1
         else:
-            return 1
-    else:
-        print("Invalid input. Please enter HIGH or LOW.")
-        return 0
+            print("Invalid input. Please enter HIGH or LOW.")
+            return 0
 
 if __name__ == "__main__":
     # Establish a single connection to Arduino
     arduino_port = '/dev/cu.usbserial-130'  # Change this to the correct port
     baud_rate = 115200
+    tracker = ArduinoTracker()
+
     time.sleep(2)
-    arduino = connect_to_arduino(arduino_port, baud_rate)
+    arduino = tracker.connect_to_arduino(arduino_port, baud_rate)
     # Connect to Arduino
     if arduino is None:
         print("Failed to connect to Arduino.\n")
 
-    if not check_connection(arduino_port, baud_rate):
+    if not tracker.check_connection(arduino_port, baud_rate):
         print("Arduino is not responding. Exiting.")
 
     while True:
